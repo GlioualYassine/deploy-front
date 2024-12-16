bafeed86a4ddc9ddd80dc1f69d208fb90ad33f84
achraf_souilah <achrafsouilah1999@gmail.com>
2024-12-16 02:20:42 +0100

    changes api

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "https://api.ramycan.com/api/v1/", // Your backend base URL  https://api.ramycan.com/api/v1/
-  baseURL: "http://localhost:8080/api/v1/", // Your backend base URL  http://localhost:8080/api/v1/
+  baseURL: "https://api.ramycan.com/api/v1/", // Your backend base URL  http://localhost:8080/api/v1/
   headers: {
     "Content-Type": "application/json",
   },
